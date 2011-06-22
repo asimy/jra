30a91fdf09364e4a1807e754649e5a3e41fff9d7
Glen Aultman-Bettridge <glen.aultmanbettridge@gmail.com>
2011-06-22 15:28:29 -0600

    Initial commit
    Beginning of Juvenile Risk Assessment system based on WSIPP JRA and TriWest Group JRA

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jra::Application.config.secret_token = '075d557a08ad50f62e9f12ed182ea8afbb86488aa624b600b326c360613319accaac02c91eaaa85efd97b6533b631feb344e9e190a12fe2b6c4e7fa61326438b'
