93ac535a1cea85630326a3f2459797570c2b0906
Glen Aultman-Bettridge <glenab@glens-olmbp.(none)>
2011-06-29 13:03:00 -0600

    testing updates

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
-describe UserController do
+describe UsersController do
+
+  # def valid_attributes
+  #   {}
+  # end
+  # 
+  # describe "GET index" do
+  #   it "assigns all users as @users" do
+  #     user = User.create! valid_attributes
+  #     get :index
+  #     assigns(:users).should eq([user])
+  #   end
+  # end
 
 end
