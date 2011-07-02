128da48132ad976e74ac6e9655a8e249ad231d75
Glen Aultman-Bettridge <glenab@glens-olmbp.(none)>
2011-07-01 22:33:25 -0600

    added nifty-generators for the easy titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
-  def title
-    base_title = "Juvenile Risk Assessment"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
+  # def title
+  #   base_title = "Juvenile Risk Assessment"
+  #   if @title.nil?
+  #     base_title
+  #   else
+  #     "#{base_title} | #{@title}"
+  #   end
+  # end
+  def title(page_title)
+    content_for(:title) {page_title}
   end
   
   def logo
