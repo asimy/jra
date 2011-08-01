c7ad2a5180f4b8b77b1d23662e8d49622b3a8b17
Glen Aultman-Bettridge <glenab@mycroft.(none)>
2011-08-01 13:16:11 -0600

    added logo helper fn, webrat ui tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  def logo
+    logo = image_tag("jra-logo.png", :alt => "Juvenile Risk Assessment", :class => "round")
+  end
+
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,28 +2,46 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   
+  before(:each) do
+    @base_title = "Juvenile Risk Assessment | "
+  end
+  
   it "should have a home page at '/'" do 
     get '/'
-    response.should have_selector('title', :content => "Juvenile Risk Assessment | Home")
+    response.should have_selector('title', :content => "#{@base_title}Home")
   end
   
   it "should have an about page at '/about'" do 
     get '/about'
-    response.should have_selector('title', :content => "Juvenile Risk Assessment | About")
+    response.should have_selector('title', :content => "#{@base_title}About")
   end
   
   it "should have a help page at '/help'" do 
     get '/help'
-    response.should have_selector('title', :content => "Juvenile Risk Assessment | Help")
+    response.should have_selector('title', :content => "#{@base_title}Help")
   end
   
   it "should have a home page at '/contact'" do 
     get '/contact'
-    response.should have_selector('title', :content => "Juvenile Risk Assessment | Contact")
+    response.should have_selector('title', :content => "#{@base_title}Contact")
   end
   
   it "should have a registration pages at '/register'" do
     get '/register'
-    response.should have_selector('title', :content => "Juvenile Risk Assessment | Register")
+    response.should have_selector('title', :content => "#{@base_title}Register")
   end
+  
+  it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      response.should have_selector('title', :content => "#{@base_title}About")
+      click_link "Help"
+      response.should have_selector('title', :content => "#{@base_title}Help")
+      click_link "Contact"
+      response.should have_selector('title', :content => "#{@base_title}Contact")
+      click_link "Home"
+      response.should have_selector('title', :content => "#{@base_title}Home")
+      click_link "Sign up now!"
+      response.should have_selector('title', :content => "#{@base_title}Register")
+    end
 end
