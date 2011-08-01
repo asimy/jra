fd7eae1fbc94c8b8d0df30d10335ce6848bf2011
Glen Aultman-Bettridge <glenab@mycroft.(none)>
2011-08-01 12:43:08 -0600

    basic formatting in place

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Jra::Application.routes.draw do
+  get "users/new"
+
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
+  match '/about', :to => 'pages#about'
   get "pages/home"
 
   get "pages/contact"
@@ -56,7 +61,7 @@ Jra::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "should have a home page at '/'" do 
+    get '/'
+    response.should have_selector('title', :content => "Juvenile Risk Assessment | Home")
+  end
+  
+  it "should have an about page at '/about'" do 
+    get '/about'
+    response.should have_selector('title', :content => "Juvenile Risk Assessment | About")
+  end
+  
+  it "should have a help page at '/help'" do 
+    get '/help'
+    response.should have_selector('title', :content => "Juvenile Risk Assessment | Help")
+  end
+  
+  it "should have a home page at '/contact'" do 
+    get '/contact'
+    response.should have_selector('title', :content => "Juvenile Risk Assessment | Contact")
+  end
+  
+end
